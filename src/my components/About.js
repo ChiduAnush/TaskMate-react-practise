9f0410e140b74945f6f56c02c8f865536dce7ffd
ChiduAnush <chiduanush1@gmail.com>
2023-03-22 15:53:26 +0530

    added an imp component

--- src/my components/About.js
+++ src/my components/About.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 export const About = () => {
+
+    let rickStyle = {
+        textDecoration: "none"
+
+    }
+
     return (
         <div className='container my-5'>
             <h5>Heyy there!</h5>
@@ -12,7 +18,8 @@ export const About = () => {
                 You can add tasks, make sure to type in both the title and description.<br />
                 And if you have finished off a task, time to delete them! yay! <br />
 
-                P.S. you wont need to worry about your tasks getting lost. It will be there even if you refresh the page :)
+                P.S. you wont need to worry about your tasks getting lost. It will be there even if you refresh the page :) <br />
+                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' style={rickStyle}>~(˘▾˘~)</a>
 
 
 
