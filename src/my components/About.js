13792bfedb3b4f563323f19121d4d3da92f44bab
ChiduAnush <chiduanush1@gmail.com>
2023-03-22 14:43:18 +0530

    updated the about page

--- src/my components/About.js
+++ src/my components/About.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
-            This is my about component
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat incidunt tempore sunt quas quidem!
-                Sequi explicabo dolorem velit ducimus, fugit id maiores molestias vitae, sed mollitia eum recusandae dolore d
-                ebitis perspiciatis ab laudantium exercitationem!</p>
+        <div className='container my-5'>
+            <h5>Heyy there!</h5>
+            <h6>Welcome to TaskMate!</h6>
+            <p>
+                One of the major things we struggle with, is remembering all the things we have to do.
+                So, I hope TaskMate will help you with that :) <br />
+
+                You can add tasks, make sure to type in both the title and description.<br />
+                And if you have finished off a task, time to delete them! yay! <br />
+
+
+
+
+
+            </p>
+
 
         </div>
     )
