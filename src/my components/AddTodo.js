998e9aa89293a802d7b88b1aefdefa1a226095e7
ChiduAnush <chiduanush1@gmail.com>
2023-03-22 11:19:51 +0530

    added the alert if any field is left empty when submitted

--- src/my components/AddTodo.js
+++ src/my components/AddTodo.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 
-export const AddTodo = () => {
+
+export const AddTodo = (props) => {
+    const [title, setTitle] = useState("")
+    const [desc, setDesc] = useState("")
+
+
+    const submit = (e) => {
+        e.preventDefault();
+        if (!title || !desc) {
+            alert("please fill out all the details")
+        }
+    }
+
     return (
         <div className='container my-3'>
             <h5>Add a task</h5>
-            <form>
+            <form onSubmit={submit}>
                 <div class="mb-3">
                     <label for="title" class="form-label">Task title</label>
-                    <input type="text" class="form-control" id="title" aria-describedby="emailHelp" />
+                    <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} class="form-control" id="title" aria-describedby="emailHelp" />
 
                 </div>
                 <div class="mb-3">
                     <label for="desc" class="form-label">Task description</label>
-                    <input type="text" class="form-control" id="desc" />
+                    <input type="text" value={desc} onChange={(e) => { setDesc(e.target.value) }} class="form-control" id="desc" />
                 </div>
 
                 <button type="submit" class="btn btn-success btn-sm">Add task</button>
